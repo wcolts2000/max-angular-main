8aace8a5fd300e9a3a4fd728e14b9b37f9c35740
sean <sean@synap.io>
2020-08-15 13:21:46 -0700

    added ingredients array to recipe details

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,22 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     @Output() recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
-            'A Test Recipe',
-            'This is simply to test functionality',
-            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+            'Tasty Schnitzel',
+            'A super-tasty Schnitzel - just awesome!',
+            'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+            [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)],
         ),
         new Recipe(
-            'Second One',
-            'Here is a description',
-            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+            'Big Fat Burger',
+            'Nothing else needed to be said!',
+            'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+            [new Ingredient('Buns', 2), new Ingredient('Ground Angus Beef', 1)],
         ),
     ];
 
